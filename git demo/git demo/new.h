81c213ff081fed732239d1c97adf6da550786982
782299528 <13693125668@163.com>
2015-11-04 12:14:33 +0800

    添加了一个类

---
+++ git demo/git demo/new.h
@@ -0,0 +1,13 @@
+//
+//  new.h
+//  git demo
+//
+//  Created by mac on 15/11/4.
+//  Copyright (c) 2015年 zhihu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface new : UITableViewCell
+
+@end
---
+++ git demo/git demo/new.m
@@ -0,0 +1,23 @@
+//
+//  new.m
+//  git demo
+//
+//  Created by mac on 15/11/4.
+//  Copyright (c) 2015年 zhihu. All rights reserved.
+//
+
+#import "new.h"
+
+@implementation new
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
